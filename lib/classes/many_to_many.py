0aee7f9b9784b69c80c163af6fee361e52368601
Adam Mwaniki <adamndegwa@protonmail.com>
2024-06-02 22:43:41 +0300

    Fixes typos

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,9 +1,12 @@
 class Article:
+    
+    all = []
 
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
         
 
     @property
@@ -12,13 +15,35 @@ class Article:
     
     @title.setter
     def title(self, value: str):
-        if hasattr(self, '_value') and self._title is not None:
+        # Inserting an extra condition to handle fringe cases like the someone declaring the title as None
+        if hasattr(self, '_title') and self._title is not None:
             raise AttributeError("Title can only be set once.")
         if isinstance(value, str) and 5<= len(value) <= 50:
             self._title = value
         else:
             raise ValueError("Title must be of type str and between 5 and 50 characters.")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self,value):
+        if isinstance(value, Author):
+            self._author = value
+        else:
+            raise ValueError("Must be of type Author")   
         
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self,value):
+        if isinstance(value, Magazine):
+            self._magazine = value
+        else:
+            raise ValueError("Must be of type Magazine")   
 class Author:
     def __init__(self, name):
         self.name = name
@@ -29,7 +54,8 @@ class Author:
     
     @name.setter
     def name(self, value: str):
-        if hasattr(self, '_value') and self._name is not None:
+        # Inserting the same extra condition as in the Article for handling None
+        if hasattr(self, '_name') and self._name is not None:
             raise AttributeError("Name can only be set once.")
         if isinstance(value, str) and len(value) > 0:
             self._name = value
@@ -62,7 +88,7 @@ class Magazine:
         if isinstance(value, str) and 2<= len(value) <= 16:
             self._name = value
         else:
-            raise ValueError("Title must be of type str and longer than 0 characters.")
+            raise ValueError("Magazine name must be of type str and longer than 0 characters.")
         
     @property
     def category(self):
@@ -73,7 +99,7 @@ class Magazine:
         if isinstance(value, str) and len(value) > 0:
             self._category = value
         else:
-            raise ValueError("Title must be of type str and longer than 0 characters.")
+            raise ValueError("Category must be of type str and longer than 0 characters.")
 
     def articles(self):
         pass
